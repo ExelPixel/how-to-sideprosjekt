51b4a45c3b118db6cf73878322931bac451d993b
ExelPixel <54321>
2024-03-20 18:08:58 +0100

    First version

---
+++ src/app/components/project-list.tsx
@@ -0,0 +1,13 @@
+export default function ProjectList() {
+    return (
+      <div>
+        <h1>liste over oppgaver/prosjekter</h1>
+        <ol>
+            <li>Oversikt over elementer</li>
+            <li>Komponenter i react</li>
+            <li>Sider og routing i Next.js</li>
+            <li>Lenker i Next.js</li>
+        </ol>
+      </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
+import ProjectList from "./components/project-list";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center h-full gap-10 p-24">
-      <h1 className="text-3xl font-medium">
-        Velkommen til {'"'}How to sideprosjekt{'"'}
-      </h1>
+    <div className="flex justify-center items-center min-h-screen bg-indigo-100 text-gray-300 px-4">
+      <title>Luka sin nettside</title>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-red-400">Luka sin nettside</h1>
+        <p className="text-red-400">Hei, jeg heter Luka og studerer datateknologi på UiB</p>
+        <img
+          className="fit-picture"
+          src="https://www.gcrieber-eiendom.no/wp-content/uploads/2021/11/Hoyteknologisenteret-GCR-02042-Foto_Tom__Jarane-3-scaled.jpg"
+          alt="Høyteknologisenteret"
+          width="500"
+          height="500"
+        />
+        <ul className="text-red-400">
+          <li>Tidlig liv</li>
+          <li>Student</li>
+          <li>Hobbyer</li>
+        </ul>
 
-      <p>
-        Om du vil ha oppgaver, les{" "}
-        <span className="bg-gray-200 font-mono py-1 px-2 rounded-md">
-          README.md
-        </span>
-        .
-      </p>
-    </main>
+        <h2 className="mt-5 text-red-400">Prosjekter på gang</h2>
+        <ProjectList/>
+      </div>
+    </div>
   );
 }
